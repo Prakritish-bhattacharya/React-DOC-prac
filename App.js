5443c1788c7d1ff1c204de6cf5e47828fd868fd6
prakritish <bhattacharyaprakritish@gmail.com>
2026-01-27 21:33:29 +0530

    NamasteDev Dummy API Use for App enhancement

--- App.js
+++ App.js
@@ -2,12 +2,29 @@ import React from "react"
 import {createRoot} from "react-dom/client"
 import { useState } from "react";
 
-const AppLayout = () =>{
-    return(
-        <div className="app_layout">
-            <Gallary/>
-        </div>
-    )
+// Our Raw Data
+const user = {
+  name: "Prakritish Bhattacharya",
+  imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
+  imageSize: 90
+}
+
+//  Our Component which fetch data from user
+const Profile = ()=>{
+  return(
+    <>
+      <h1>{user.name}</h1>
+      <img
+          className= "avatar"
+          src = {user.imageUrl}
+          alt = {"Photo of" + user.name}
+          style = {{
+            width: user.imageSize,
+            height: user.imageSize
+          }}
+      ></img>
+    </>
+  )
 }
 const root = createRoot(document.getElementById("root"))
-root.render(<AppLayout/>)
+root.render(<Profile/>)
---
+++ Component/Button.js
@@ -0,0 +1,9 @@
+const  Button = ()=>{
+    return(
+        <div>
+            <h1>Welcome to my app</h1>
+            
+        </div>
+    )
+}
+export default Button
